cb1949627cbb372141e8cbc3bb6fe02f659b6607
Francis Maina <sebastianfrancix@gmail.com>
2023-01-02 14:55:07 +0300

    added error highlighting to the input if the color passed is invalid

--- src/App.js
+++ src/App.js
@@ -10,7 +10,13 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("handle Submit func");
+    try {
+      let colors = new Values(color).all(10);
+      console.log(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
   };
 
   return (
@@ -18,7 +24,17 @@ function App() {
       <section className="container">
         <h3>Color Generator</h3>
         <form onSubmit={handleSubmit}>
-          <input type="text" value={color} />
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#ffbd33"
+            // set error highlight color
+            className={`${error ? "error" : null}`}
+          />
+          <button className="btn" type="submit">
+            Submit
+          </button>
         </form>
       </section>
       <section className="colors">
