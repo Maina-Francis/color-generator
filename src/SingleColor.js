a8708de29f075a383bc22f265d030e414ab60c28
Francis Maina <sebastianfrancix@gmail.com>
2023-01-04 02:24:03 +0300

    added functionality to copy my hex value to the clipboard

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -8,13 +8,26 @@ const SingleColor = ({ index, weight, rgb }) => {
 
   const hex = rgbToHex(...rgb);
 
+  //   clear 'copied to clipboard' alert message after 3 seconds
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, [alert]);
+
   return (
     <article
       className={`color ${index > 10 && "color-light"}`}
       style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hex);
+      }}
     >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">{hex}</p>
+      {alert && <p className="alert">Copied to clipboard</p>}
     </article>
   );
 };
